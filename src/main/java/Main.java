661cfb8da600e85776979e20dcbcb9f0d51ba4d9
Brendanej <56318922+Brendanej@users.noreply.github.com>
2020-11-22 23:35:47 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
@@ -12,15 +8,17 @@ import org.joda.time.*;
 public class Main {
     
     public static void main(String[] args) {
-        try{
+        //Making Students and their info
         Student Student1 = new Student("Brendan",15,"17/5/2005",1);
         Student Student2 = new Student("Sarah",17,"6/2/2003",2);
         Student Student3 = new Student("Peter",17,"8/09/2000",3);
-            
+        //making Module and their info
         Module EE356 = new Module("EE456", "1", new Student[]{Student1, Student2},"EE355 and EE354");
         Module EE355 = new Module("EE455", "2", new Student[]{Student1, Student3},"EE356 and EE354");
         Module EE354 = new Module("EE454", "3", new Student[]{Student1, Student3, Student2},"EE355 and EE356");
+        //Making Course and their info
         Course ECE = new Course("ECE", new Module[]{EE356, EE355, EE354},new DateTime("2016-02-08"), new DateTime("2020-08-04"),new Student[]{Student1});
+        //print outs
         System.out.println("Courses : "+ECE.getCourseName());
         System.out.println("Modules : "+ EE356.getModuleName()+ " " + EE355.getModuleName()+ " "+EE354.getModuleName());
         System.out.println("Students : "+ Student1.getName()+ " "+Student2.getName() +" "+Student3.getName());
@@ -35,8 +33,5 @@ public class Main {
                 +" StartDate :"+ECE.getEndDate()+".");
     }
     
-    catch(Exception e){
-    }
 
-}
 }
